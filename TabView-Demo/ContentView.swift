611ef09941270c764fda0bca15357da67179c364
Yüksel Toprak <yuekseltoprak@gmail.com>
2021-10-07 16:15:22 +0300

    Initial Commit

---
+++ TabView-Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabView-Demo
+//
+//  Created by Yüksel Toprak on 07.10.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TabView-Demo/TabView_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabView_DemoApp.swift
+//  TabView-Demo
+//
+//  Created by Yüksel Toprak on 07.10.21.
+//
+
+import SwiftUI
+
+@main
+struct TabView_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
